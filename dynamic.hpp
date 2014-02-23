853d5285f1b3b2092ddb68dbac720e0e8decda9c
longshadian <longshadian@gmail.com>
2014-02-23 21:26:35 +0800

    dynamic

--- dynamic.hpp
+++ dynamic.hpp
@@ -1,12 +1,28 @@
-#ifndef ESTL_DYNAMIC
-#define ESTL_DYNAMIC
+#ifndef ESTL_DYNAMIC_H
+#define ESTL_DYNAMIC_H
 
+#include <iostream>
+#include <cstring>
 #include <map>
 #include <string>
 #include <vector>
 
 namespace estl
 {
+
+#define ESTL_DYNAMIC_APPLY(type, apply)         \
+do {                                           \
+    switch ((type)) {                          \
+    case NULLT : apply(void*); break;        \
+    case DOUBLE: apply(double); break;         \
+    case UINT : apply(unsigned); break;        \
+    case ARRAY : apply(Array);  break;         \
+    case OBJECT : apply(Map); break;            \
+    case BOOL : apply(bool); break;             \
+    case STRING : apply(std::string); break;    \
+    case default : apply(bool); abort();        \
+    }                                           \
+} while (0)
 
 class Dynamic
 {
@@ -16,6 +32,7 @@ public:
         NULLT,
         DOUBLE,
         UINT,
+        INT,
         ARRAY,
         OBJECT,
         BOOL,
@@ -27,12 +44,78 @@ public:
         new (&mData.mString) std::string();
     }
 
-    ~Dynamic()
+    Dynamic(int v)
+        : mType(Type::INT)
     {
-        mData.mString.~basic_string();
+        mData.mInt = v;
     }
 
-    std::
+    Dynamic(unsigned v)
+        : mType(Type::UINT)
+    {
+        mData.mUInt = v;
+    }
+
+    Dynamic(double v)
+        : mType(Type::DOUBLE)
+    {
+        mData.mDouble = v;
+        std::cout << v << std::endl;
+    }
+    
+    Dynamic(std::string name)
+        : mType(Type::STRING)
+    {
+        mData.mString = name;
+    }
+
+   Dynamic(const char* name) 
+       : mType(Type::STRING)
+   {
+       mData.mString = name;
+   }
+
+    ~Dynamic()
+    {
+        if (mType == Type::STRING)
+            mData.mString.~basic_string();
+    }
+
+    Dynamic(const Dynamic& obj)
+        : mType(Type::OBJECT)
+    {
+
+    }
+
+    Dynamic& operator= (const Dynamic& obj)
+    {
+        mType = obj.mType;
+        std::memcpy(&mData, &obj.mData, sizeof(Data));
+        return *this;
+    }
+
+    Dynamic& operator[] (const Dynamic& obj)
+    {
+        return mData.mMap[obj];
+    }
+
+    bool operator< (const Dynamic& obj) const
+    {
+        if (mType != obj.mType)
+            return mType < obj.mType;
+#define ESTL_X(T)   return false;
+        ESTL_X(T)
+#undef ESTL_X
+    }
+
+    bool as_bool() const { return mData.mBool; }
+    int as_int() const { return mData.mInt; }
+    unsigned as_uint() const { return mData.mUInt; }
+    double as_double() const { return mData.mDouble; }
+    std::string as_string() const { return mData.mString; }
+private:
+    using Array = std::vector<Dynamic>;
+    using Map = std::map<Dynamic, Dynamic>;
 private:
     Type    mType;
     union Data
@@ -40,11 +123,13 @@ private:
         Data() {}
         ~Data() {}
 
+        bool    mBool;
+        unsigned mUInt;
         int     mInt;
         double  mDouble;
         std::string mString;
-        std::map<Dynamic, Dynamic> mMap;
-        std:vector<Dynamic> mArray;
+        Map mMap;
+        Array mArray;
     } mData;
 
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 #include "dynamic.hpp"
 
 int main(int argc, char** argv)
 {
     estl::Dynamic d;
+    d = 12;
+    d = 123;
+    d = 1234.4133;
+    std::cout << d.as_double() << std::endl;
 
-    std::vector<int> v = {1, 2, 3, 4};
-    for (auto i : v)
-        std::cout << i << std::endl;
+    //std::map<estl::Dynamic, estl::Dynamic> m = {{"a", "123"}, {"b", "456"}};
+    //std::cout << m.size() << std::endl;
 
 	return 0;
 }
