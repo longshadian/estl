4e263fae0ebd3c5b2870f4a78a8201152a1945ca
cgy <guangyuanchen001@163.com>
2019-12-27 10:08:09 +0800

    delete python script

--- scripts/python/auto_restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. /etc/profile
-
-cd /mnt/data/zw/tbj_server/kaixinmaxituantbj
-python36 ./auto_restart.py >> ./temp/auto_restart.log 2>&1
--- scripts/python/rand_passwd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-python rand_passwd.py
-
-pause
