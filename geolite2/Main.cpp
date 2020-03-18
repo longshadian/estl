06a160114c696687a7c6d52a28389dc8cf0c0cec
cgy <guangyuanchen001@163.com>
2020-03-18 14:41:50 +0800

    update geolite2

--- geolite2/Main.cpp
+++ geolite2/Main.cpp
@@ -7,17 +7,20 @@
 #include <maxminddb.h>
 
 void IPV4_to_Addr(const char* p, IN_ADDR* addr);
-int LookupIPV4(MMDB_s* mmdb, const std::string& ip, std::string& contry_code, std::string& contry_name);
-bool LookupSockInAddr4CountryCode(MMDB_s* mmdb, const IN_ADDR& remote_addr, std::string* contry_code, std::string* contry_name);
-bool GeoDbGetCountryData(MMDB_entry_s* db_entry, std::string* country_code, std::string* country_name);
-bool GeoDbGetContinentData(MMDB_entry_s* db_entry, std::string* continent_code, std::string* continent_name);
+
+bool Lookup_City(MMDB_s* mmdb, const IN_ADDR& remote_addr, std::string* city_name);
+bool Lookup_Country(MMDB_s* mmdb, const IN_ADDR& remote_addr, std::string* country_code, std::string* country_name);
+
+bool Geo_GetCountryData(MMDB_entry_s* db_entry, std::string* country_code, std::string* country_name);
+bool Geo_GetContinentData(MMDB_entry_s* db_entry, std::string* continent_code, std::string* continent_name);
+bool Geo_GetCityData(MMDB_entry_s* db_entry, std::string* city_name);
 
 void IPV4_to_Addr(const char* p, IN_ADDR* addr)
 {
     addr->S_un.S_addr = inet_addr(p);
 }
 
-int LookupIPV4(MMDB_s* mmdb, const std::string& ip, std::string& contry_code, std::string& contry_name)
+int LookupIPV4_Country(MMDB_s* mmdb, const std::string& ip, std::string& country_code, std::string& country_name)
 {
     if (ip.empty())
         return -1;
@@ -25,13 +28,27 @@ int LookupIPV4(MMDB_s* mmdb, const std::string& ip, std::string& contry_code, st
     IN_ADDR addr;
     IPV4_to_Addr(ip.c_str(), &addr);
 
-    bool succ = LookupSockInAddr4CountryCode(mmdb, addr, &contry_code, &contry_name);
+    bool succ = Lookup_Country(mmdb, addr, &country_code, &country_name);
     if (!succ)
         return -1;
     return 0;
 }
 
-bool LookupSockInAddr4CountryCode(MMDB_s* mmdb, const IN_ADDR& remote_addr, std::string* contry_code, std::string* contry_name)
+int LookupIPV4_City(MMDB_s* mmdb, const std::string& ip, std::string& country_name)
+{
+    if (ip.empty())
+        return -1;
+    std::string name;
+    IN_ADDR addr;
+    IPV4_to_Addr(ip.c_str(), &addr);
+
+    bool succ = Lookup_City(mmdb, addr, &country_name);
+    if (!succ)
+        return -1;
+    return 0;
+}
+
+bool Lookup_Country(MMDB_s* mmdb, const IN_ADDR& addr, std::string* country_code, std::string* country_name)
 {
     /*
     if (IN4_IS_ADDR_UNSPECIFIED(&RemoteAddress) || IN4_IS_ADDR_LOOPBACK(&RemoteAddress)) {
@@ -46,19 +63,38 @@ bool LookupSockInAddr4CountryCode(MMDB_s* mmdb, const IN_ADDR& remote_addr, std:
     std::memset(&mmdb_result, 0, sizeof(MMDB_lookup_result_s));
 
     ipv4sa.sin_family = AF_INET;
-    ipv4sa.sin_addr = remote_addr;
+    ipv4sa.sin_addr = addr;
     mmdb_result = ::MMDB_lookup_sockaddr(mmdb, (PSOCKADDR)&ipv4sa, &mmdb_error);
 
     if (mmdb_error == 0 && mmdb_result.found_entry) {
-        if (GeoDbGetCountryData(&mmdb_result.entry, contry_code, contry_name))
+        if (Geo_GetCountryData(&mmdb_result.entry, country_code, country_name))
             return true;
-        if (GeoDbGetContinentData(&mmdb_result.entry, contry_code, contry_name))
+        if (Geo_GetContinentData(&mmdb_result.entry, country_code, country_name))
             return true;
     }
     return false;
 }
 
-bool GeoDbGetCountryData(MMDB_entry_s* db_entry, std::string* country_code, std::string* country_name)
+bool Lookup_City(MMDB_s* mmdb, const IN_ADDR& remote_addr, std::string* city_name)
+{
+    MMDB_lookup_result_s mmdb_result;
+    SOCKADDR_IN ipv4sa;
+    int mmdb_error = 0;
+    std::memset(&ipv4sa, 0, sizeof(SOCKADDR_IN));
+    std::memset(&mmdb_result, 0, sizeof(MMDB_lookup_result_s));
+
+    ipv4sa.sin_family = AF_INET;
+    ipv4sa.sin_addr = remote_addr;
+    mmdb_result = ::MMDB_lookup_sockaddr(mmdb, (PSOCKADDR)&ipv4sa, &mmdb_error);
+
+    if (mmdb_error == 0 && mmdb_result.found_entry) {
+        if (Geo_GetCityData(&mmdb_result.entry, city_name))
+            return true;
+    }
+    return false;
+}
+
+bool Geo_GetCountryData(MMDB_entry_s* db_entry, std::string* country_code, std::string* country_name)
 {
     MMDB_entry_data_s data_entry;
     std::string code;
@@ -85,7 +121,27 @@ bool GeoDbGetCountryData(MMDB_entry_s* db_entry, std::string* country_code, std:
     return false;
 }
 
-bool GeoDbGetContinentData(MMDB_entry_s* db_entry, std::string* continent_code, std::string* continent_name)
+bool Geo_GetCityData(MMDB_entry_s* db_entry, std::string* city_name)
+{
+    MMDB_entry_data_s data_entry;
+    std::string code;
+    std::string name;
+
+    if (MMDB_get_value(db_entry, &data_entry, "city", "names", "en", NULL) == MMDB_SUCCESS) {
+        if (data_entry.has_data && data_entry.type == MMDB_DATA_TYPE_UTF8_STRING) {
+            //name = PhConvertUtf8ToUtf16Ex((PCHAR)mmdb_entry.utf8_string, mmdb_entry.data_size);
+            name.assign(data_entry.utf8_string, data_entry.data_size);
+        }
+    }
+
+    if (!name.empty()) {
+        *city_name = name;
+        return true;
+    }
+    return false;
+}
+
+bool Geo_GetContinentData(MMDB_entry_s* db_entry, std::string* continent_code, std::string* continent_name)
 {
     MMDB_entry_data_s data_entry;
     std::string code;
@@ -116,7 +172,8 @@ bool GeoDbGetContinentData(MMDB_entry_s* db_entry, std::string* continent_code, 
 
 int Test()
 {
-    std::string db_file = "C:/Users/admin/Desktop/geolite2/GeoLite2-Country_20200303/GeoLite2-Country.mmdb";
+    //std::string db_file = "C:/Users/admin/Desktop/geolite2/GeoLite2-Country_20200303/GeoLite2-Country.mmdb";
+    std::string db_file = "C:/Users/admin/Desktop/geolite2/GeoLite2-City_20200317/GeoLite2-City.mmdb";
     MMDB_s mmdb_;
     MMDB_s* mmdb = &mmdb_;
     int status = ::MMDB_open(db_file.c_str(), MMDB_MODE_MMAP, mmdb);
@@ -135,10 +192,14 @@ int Test()
 
     for (size_t i = 0; i != vec.size(); ++i) {
         std::string ip = vec[i];
-        std::string code;
-        std::string name;
-        LookupIPV4(mmdb, ip, code, name);
-        printf("ip: [%s] \t\t code: [%s] \t\t name: [%s]\n", ip.c_str(), code, name);
+        std::string country_code;
+        std::string country_name;
+        LookupIPV4_Country(mmdb, ip, country_code, country_name);
+
+        std::string city_name;
+        LookupIPV4_City(mmdb, ip, city_name);
+        printf("ip: [%s] \t country: [%s] [%s] \t\t city: [%s]\n"
+            , ip.c_str(), country_code.c_str(), country_name.c_str(), city_name.c_str());
     }
 
     ::MMDB_close(mmdb);
