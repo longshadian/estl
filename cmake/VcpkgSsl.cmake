06468b9e816443682f499d824db985579933a8c7
cgy <guangyuanchen001@163.com>
2020-05-06 09:15:58 +0800

    update cmake

---
+++ cmake/VcpkgSsl.cmake
@@ -0,0 +1,28 @@
+
+#### 获取ssl后缀
+#### 暂时只支持x64-windows/x86-windows
+function(check_vcpkg_ssl_postfix out_POSTFIX)
+    if(CMAKE_BUILD_TYPE MATCHES "Debug")
+        set(${out_POSTFIX} ".lib")
+    elseif(CMAKE_BUILD_TYPE MATCHES "Release")
+        set(${out_POSTFIX} ".lib")
+    else()
+        set(${out_POSTFIX} ".lib")
+    endif()
+
+    set(${out_POSTFIX} ${${out_POSTFIX}} PARENT_SCOPE)
+endfunction()
+
+#### 找到zlib库的路径
+function(check_vcpkg_ssl_lib LIB_NAME VCPKG_LIB_DIR out_LIB)
+    check_vcpkg_ssl_postfix(VCPKG_SSL_POSTFIX)
+    set(LIB_FULLNAME ${LIB_NAME}${VCPKG_SSL_POSTFIX})
+
+    find_library(${out_LIB} ${LIB_FULLNAME} PATHS ${VCPKG_LIB_DIR})
+    if(NOT ${out_LIB})
+        message(FATAL_ERROR "Can't find  ${LIB_FULLNAME} ${VCPKG_LIB_DIR}")
+    endif()
+
+    set(${out_LIB} ${${out_LIB}} PARENT_SCOPE)
+    #message("find ${LIB_FULLNAME} ${out_LIB}")
+endfunction()
