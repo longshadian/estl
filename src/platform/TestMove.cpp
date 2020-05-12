1c4b49f706a24b6d1514e74fdbb92d216fc2ab32
cgy <guangyuanchen001@163.com>
2020-05-12 15:00:42 +0800

    增加TestMove.cpp

---
+++ src/platform/TestMove.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+#include <string>
+#include <utility>
+
+#include "Common.h"
+
+#include "../doctest/doctest.h"
+
+namespace test_move
+{
+
+struct XString
+{
+public:
+    XString(): m_() {}
+    XString(const char* m) : m_(m) { }
+    XString(const std::string& m) : m_(m) { }
+
+    ~XString()
+    {
+    }
+
+    XString(const XString& rhs) = default;
+    XString& operator=(const XString& rhs) = default;
+
+    XString(XString&& rhs)
+        : m_(std::move(rhs.m_))
+    {
+        std::cout << "XString move copy constructor\n";
+    }
+
+    XString& operator=(XString&& rhs)
+    {
+        std::cout << "XString move assigment constructor\n";
+        if (this != &rhs) {
+            std::swap(m_, rhs.m_);
+        }
+        return *this;
+    }
+
+    std::ostream& operator<<(std::ostream& os) const
+    {
+        os << this->m_;
+        return os;
+    }
+
+    bool operator==(const std::string& s) const
+    {
+        return m_ == s;
+    }
+
+    bool operator!=(const std::string& s) const
+    {
+        return (*this) != s;
+    }
+
+    std::string m_;
+};
+
+class X
+{
+public:
+    X()
+    {
+    }
+
+    ~X()
+    {
+    }
+
+    std::string str_;
+    XString xstr_;
+
+    const std::string& get_str() const & { return str_; } 
+    std::string& get_str() & { return str_; } 
+    std::string&& get_str() && { return std::move(str_); }
+
+    const XString& get_xstr() const & { return xstr_; } 
+    XString& get_xstr() & { return xstr_; } 
+    //XString&& get_xstr() && { return std::move(xstr_); }
+    XString get_xstr() && { return std::move(xstr_); }
+};
+
+int TestMove()
+{
+    const std::string s = "std::string";
+    const std::string xs = "xstring";
+    X x;
+    x.str_ = s;
+    x.xstr_ = xs;
+
+    auto s0 = x.get_str();
+    CHECK(s0 == s);
+    CHECK(x.get_str() == s);
+
+    auto s1 = std::move(x).get_str();
+    CHECK(s1 == s);
+    CHECK(x.get_str() == "");
+
+    auto xs0 = x.get_xstr();
+    CHECK(xs0 == xs);
+    CHECK(x.get_xstr() == xs);
+    auto xs1 = std::move(x).get_xstr();
+    CHECK(xs1 == xs);
+    CHECK(x.get_xstr() == "");
+
+    return 0;
+}
+
+} // namespace test_move
+
+#if 1
+TEST_CASE("TestMove")
+{
+    DPrintf("TestMove");
+    CHECK(test_move::TestMove() == 0);
+}
+#endif
+
---
+++ src/platform/TestStringView.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+#include <string>
+#include <utility>
+
+#include "Common.h"
+
+#include "../doctest/doctest.h"
+
+namespace test_string_view
+{
+
+std::string_view substr_by_range(std::string_view sv, std::size_t b, std::size_t e)
+{
+    return sv.substr(b, e-b);
+}
+
+int TestFind()
+{
+std::string_view s = R"(
+[client 192.168.106.1] 
+ModSecurity: Access denied with code 403 (phase 2).
+Matched "Operator `Rx' with parameter `^[\d.:]+$' against variable `REQUEST_HEADERS:Host' (Value: `192.168.106.200:11101' )
+[file "conf / rules / REQUEST - 920 - PROTOCOL - ENFORCEMENT.conf"]
+[line "722"] [id "920350"] [rev ""] 
+[msg "Host header is a numeric IP address"] 
+[data "192.168.106.200 : 11101"] [severity "4"] [ver "OWASP_CRS / 3.2.0"] [maturity "0"] [accuracy "0"] 
+[tag "application - multi"] [tag "language - multi"] [tag "platform - multi"] 
+[tag "attack - protocol"] [tag "paranoia - level / 1"] [tag "OWASP_CRS"] 
+[tag "OWASP_CRS / PROTOCOL_VIOLATION / IP_HOST"] [tag "WASCTC / WASC - 21"] 
+[tag "OWASP_TOP_10 / A7"] [tag "PCI / 6.5.10"] 
+[hostname "0.0.0.0"] [uri " / "] [unique_id "158901782897.422198"] [ref "o0, 21v21, 21"]
+)";
+
+    {
+        std::string_view b = "[file \"";
+        std::string_view e = "\"]";
+        auto pb = s.find(b);
+        auto pe = s.find(e);
+        std::string sv = std::string(substr_by_range(s, pb +b.length(), pe));
+        DPrintf("sv: ==>%s<==", sv.c_str());
+    }
+    return 0;
+}
+
+
+} // namespace test_string_view
+
+//#define USE_TEST
+
+#if defined (USE_TEST)
+TEST_CASE("TestStringView")
+{
+    DPrintf("TestStringView");
+    CHECK(test_string_view::TestFind() == 0);
+}
+#endif
