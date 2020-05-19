821b24dc5c1b9fbf9c35083ba9ea6eea040b09b2
cgy <guangyuanchen001@163.com>
2020-05-19 17:01:59 +0800

    update enoki

---
+++ src/platform/TestConditionVariable.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+#include <string>
+#include <utility>
+#include <list>
+#include <functional>
+#include <condition_variable>
+
+#include "Common.h"
+
+#include "../doctest/doctest.h"
+
+namespace test_cond
+{
+
+struct X
+{
+    X()
+    : running_()
+    , thd_()
+    , mtx_()
+    , cnd_()
+    , buffer_()
+    {
+
+    }
+
+    ~X()
+    {
+        running_ = false;
+        if (thd_.joinable())
+            thd_.join();
+    }
+
+    void init()
+    {
+        running_ = true;
+        std::thread tmp(std::bind(&X::run, this));
+        thd_ = std::move(tmp);
+    }
+
+    void post(std::string s)
+    {
+        std::lock_guard<std::mutex> lk{mtx_};
+        buffer_.emplace_back(std::move(s));
+        cnd_.notify_all();
+    }
+
+    void run()
+    {
+        while (running_) {
+            std::string s;
+            {
+                std::unique_lock<std::mutex> lk{mtx_};
+                auto rv = cnd_.wait_for(lk, std::chrono::seconds(100), [this](){ return !buffer_.empty(); });
+                if (buffer_.empty()) {
+                    std::cout << "buffer empty\n";
+                    continue;
+                }
+                s = std::move(buffer_.front());
+                buffer_.pop_front();
+                std::cout << "buffer: " << s << "\n";
+            }
+        }
+    }
+
+    bool                    running_;
+    std::thread             thd_;
+    std::mutex              mtx_;
+    std::condition_variable cnd_;
+    std::list<std::string>  buffer_;
+};
+
+
+static int Test1()
+{
+    X x{};
+    x.init();
+
+    int n = 0;
+    while (1) {
+        std::this_thread::sleep_for(std::chrono::seconds{2});
+        x.post(std::to_string(++n));
+        if (n == 5) {
+            break;
+        }
+    }
+
+    x.cnd_.notify_all();
+    return 0;
+}
+
+} // namespace test_cond
+
+#if 1
+TEST_CASE("TestCondition")
+{
+    DPrintf("TestCondition");
+    CHECK(test_cond::Test1() == 0);
+}
+#endif
+
---
+++ test/test-enoki/Enoki.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cassert>
+#include <sstream>
+
+#include <enoki/array.h>
+#include <enoki/matrix.h>
+
+using Float = float;
+using Vector3f = enoki::Array<Float, 3>;
+using Matrix3f = enoki::Matrix<Float, 3>;
+
+/// Generic string conversion routine
+template <typename T> inline std::string to_string(const T& value) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+void Test()
+{
+    Matrix3f m(
+        1, 2, 3,
+        4,5,6,
+        7,8,9);
+    Vector3f v(1,2,3);
+
+    std::cout << to_string(m) << "\n";
+    std::cout << to_string(v) << "\n";
+    std::cout << to_string(m*v) << "\n";
+}
+
+int main()
+{
+    if (1) {
+        Test();
+    }
+    system("pause");
+    return 0;
+}
