faa7b09e7bf3c8586c03043d4015eb41d45dcc88
bolan <bolan@bolan.com>
2020-12-15 20:48:17 +0800

    templates

---
+++ templates/src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Person.h"
+
+int main()
+{
+    TestPerson();
+    return 0;
+}
+
---
+++ templates/src/Person.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <iostream>
+
+class Person
+{
+    std::string name;
+public:
+
+    template <typename T>
+    Person(T&& t)
+        : name(std::forward<T>(t))
+    {
+        std::cout << "TMPL-CONSTR Person: " << name << "\n";
+    }
+
+    ~Person()
+    {
+    }
+
+    Person (Person const& p) : name(p.name) {
+        std::cout << "COPY-CONSTR Person: " << name << "\n";
+    }
+
+    Person (Person&& p) : name(std::move(p.name)) {
+        std::cout << "MOVE-CONSTR Person: " << name << "\n";
+    }
+};
+
+inline void TestPerson()
+{
+    std::string s1 = "str1";
+    Person p1(s1);
+    Person p2("temp");
+    Person p3(p1);
+}
+
