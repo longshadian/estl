a401242375550454e4c3072dc3064b9e0456ce62
cgy <guangyuanchen001@163.com>
2021-02-03 11:56:49 +0800

    boost_cmake支持vcpkg目录设置

--- boost_cmake/build.bat
+++ boost_cmake/build.bat
@@ -1,6 +1,9 @@
 @ECHO off
 
+::echo %WINDIR%
+::echo %AppData%
+
 cd /d %~dp0
-"C:\Program Files\CMake\bin\cmake.exe" -DCMAKE_BUILD_TYPE=Debug -DUSE_VCPKG=1 -B "build"
+"C:\Program Files\CMake\bin\cmake.exe" -DCMAKE_BUILD_TYPE=Debug -DVCPKG_DIR="D:/vcpkg" -B "build"
  
 pause 
